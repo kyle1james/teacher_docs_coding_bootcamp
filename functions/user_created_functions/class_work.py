dd04b6f76dc73ea3187d09cb3040d8c23e5013bb
Kyle <kyle1james@gmail.com>
2018-10-17 15:19:29 -0400

    built-in-functions done

---
+++ functions/user_created_functions/class_work.py
@@ -0,0 +1,70 @@
+# after you test your code, comment it out with a #
+
+################ Defining a function #######################################################
+
+# create a function called hello
+# take in a name as input
+# return a special hello message with the user's name
+
+
+
+
+################ Calling a function  ##################################################################
+
+# call your function here
+
+
+################ Multiple-argument Methods  ###########################################################
+
+
+# create a function that takes in two numbers as arguments
+# output the sum of the two numbers
+
+
+################ Functions With Multiple Returns ########################################################
+
+# age
+
+# create a function that takes in a user's height as an argument
+# output what roller coaster rides the user may ride based on their height
+
+
+
+################ Labs  #################################################################################
+
+# lift_off
+
+# create a function that takes in weight and fuel in pounds
+# output True or False if your spaceship can make it to orbit
+# conditions: 1 pound of weight uses 9.33 pounds of rocket fuel
+
+# Mild: return how much more fuel you need to lift_off or how much extra fuel you have
+
+# Spicy: return how many 180 pound humans you can bring on your trip
+
+
+# what_to_eat
+
+# create a function that takes in the weather: sunny, rainy, cold, and hot
+# output what type of food you should eat based on the weather
+
+# mild: add temperature as an additional argument
+
+# spicy: add temperature and time of day as arguments
+
+
+# who_am_i
+
+# create a function that asks your user several questions
+# output which fictional character the user is based on their answers to the questions
+# examples: superhero, hogwarts house, etc.
+
+# mild: have at least 5 output options
+
+# spicy: use Multiple conditional statements such as 'and', 'or', 'not'
+
+
+
+################ Create Your Own function  #######################################################
+
+# create a function for a classmate
