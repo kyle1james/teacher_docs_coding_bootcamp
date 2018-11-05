202302ac1eaf1f739a93602f672f674e2f68b704
Kyle <kyle1james@gmail.com>
2018-11-05 12:27:43 -0500

    stuff

---
+++ datatypes/lab.py
@@ -0,0 +1,30 @@
+################### Practice ###########################################################################
+# add your age to the and save the sum in a variable
+
+# subtract your birth year from this year and save the difference in a variable
+
+# multiply your age by the year and save the product in a variable
+
+# divide your age by your birth year save the quotient in a variable
+
+
+################### Lab One ###########################################################################
+# create a bank acount variable
+
+# create a monthly_bills variable
+
+# create a monthly_income variable
+
+# find yearly bills cost
+
+# find yearly income
+
+# find the end of the year back_account total
+
+# how much money will you have in 10 years?
+
+# what if your monthly_income doubles every five years?
+
+# what if you have kids that cost 100000 a year?
+
+################### Lab Two ###########################################################################
