c13a8b05ee387610f779b709f44889af4d7a7a19
Kyle <kyle1james@gmail.com>
2018-12-19 08:04:09 -0500

    meow

--- 03_conditionals/lab.py
+++ 03_conditionals/lab.py
@@ -1,7 +1,15 @@
 ########################### Panic Zone #######################################################################################################################################################
-# Create a program that suggest what type of food you should eat
-# Based on the weather. For example, if it is hot have hamburgers, else have soup
-# Add in location to take this to the next level
+# Create a program that gives a sassy response to a user's age
+# input: user age
+# output: sassy response based on user age
+
+
+########################### Buzzfeed Quiz #######################################################################################################################################################
+# Create a program that is like a buzzfeed quiz
+# What Hogwarts house do you belong in
+# What superhero are you
+# What tv/ movie character are you?
+# a program that suggest what type of food you should eat
 
 ########################### Password Login #######################################################################################################################################################
 # For your lab, you will create a login for a user. We want to plan this program out, then complete the lab.
@@ -12,6 +20,3 @@
     # Have a user set a password
     # Have user try to login using password
     # Spicy: try using a username and password
-
-########################### Spicy #######################################################################################################################################################
-# Move on to labs in day 4
